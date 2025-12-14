8e421513feafdf4245cde806878e501e91ba2e61
liudmylaber <liudmyla.berdychevska@gmail.com>
2025-12-15 00:05:32 +0200

    Fix database connection using env variables

--- db.js
+++ db.js
@@ -1,10 +1,18 @@
 const mysql = require('mysql2');
+require('dotenv').config(); 
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',     
-    database: 'blog_db',
-    password: 'admin'   
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD, 
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    },
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 module.exports = pool.promise();
